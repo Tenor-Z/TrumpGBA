d2c1f84d35a63b9e71e8dfe1df2b389997ed6c55
Tyler Bifolchi <55516164+Tenor-Z@users.noreply.github.com>
2023-09-25 22:48:32 -0400

    Added files

---
+++ build/gbatrump.d
@@ -0,0 +1,19 @@
+gbatrump.o: \
+ C:/Users/TJBif/Desktop/gba-examples-master/graphics/GBATrump/source/gbatrump.c \
+ C:/devkitpro/libgba/include/gba_video.h \
+ C:/devkitpro/libgba/include/gba_base.h \
+ C:/devkitpro/libgba/include/gba_types.h \
+ C:/devkitpro/libgba/include/gba_systemcalls.h \
+ C:/devkitpro/libgba/include/gba_input.h \
+ C:/devkitpro/libgba/include/gba_interrupt.h \
+ C:/devkitpro/libgba/include/pcx.h C:/devkitpro/libgba/include/fade.h \
+ C:/Users/TJBif/Desktop/gba-examples-master/graphics/GBATrump/build/trump_pcx.h
+C:/devkitpro/libgba/include/gba_video.h:
+C:/devkitpro/libgba/include/gba_base.h:
+C:/devkitpro/libgba/include/gba_types.h:
+C:/devkitpro/libgba/include/gba_systemcalls.h:
+C:/devkitpro/libgba/include/gba_input.h:
+C:/devkitpro/libgba/include/gba_interrupt.h:
+C:/devkitpro/libgba/include/pcx.h:
+C:/devkitpro/libgba/include/fade.h:
+C:/Users/TJBif/Desktop/gba-examples-master/graphics/GBATrump/build/trump_pcx.h:
